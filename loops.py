e30b74ae54018d38b694ffe8b28f88c1e0f57cfd
aditya kumar <ag14012008@gmail.com>
2023-01-02 11:23:58 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+for i in [1,2,3,4,5]:
+    print(i)
+
+for i in range(6):
+    print(i)
+
+names = ["aditya","ram","shyam","mohan"]
+for name in names :
+    print(name)
+
+name = "harry"
+
+for character in name:
+    print(character)
