bcc5681d2a9f0b7c7ad022d70cd59fcf14583684
aditya kumar <ag14012008@gmail.com>
2023-01-05 00:08:19 +0530

    added lambda insertion

--- lambda.py
+++ lambda.py
@@ -1,6 +1,4 @@
 people = [{"name" : "harry", "house" : "gryffindor"},{"name" : "ram" , "house" : "ayodha"}, {"name":"shyam","house":"brindavan"}]
 
-def f (person):
-    return person["name"]
-people.sort()
+people.sort(key= lambda person : person ["house"])
 print(people)
