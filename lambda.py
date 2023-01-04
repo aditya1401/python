c2cbf921ad7142052209b296798c20c83d887bbb
aditya kumar <ag14012008@gmail.com>
2023-01-05 00:03:55 +0530

    added funtion to sort name

--- lambda.py
+++ lambda.py
@@ -1,3 +1,6 @@
 people = [{"name" : "harry", "house" : "gryffindor"},{"name" : "ram" , "house" : "ayodha"}, {"name":"shyam","house":"brindavan"}]
+
+def f (person):
+    return person["name"]
 people.sort()
 print(people)
